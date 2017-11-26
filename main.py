11fea25efd7cd80c605ad88d1b1697df1a918606
Daniel <danielf.tapiar@gmail.com>
2017-11-26 16:17:36 +0000

    threading changes

--- main.py
+++ main.py
@@ -18,35 +18,30 @@ post_fields = {'foo': 'bar'}     # Set POST fields here
 
 url_rfid = url
 url_gsm = url
-url_odb2 = url
+url_obd2 = url
 
+def postToServer(data, url, key):
+    data_json = json.dumps(data) 
+    payload = data 
+    r = requests.post(url, json={key: payload}) 
 
 def worker(num):
 	if(num == 1): #GSM
-		print("GSM MODULE")
-		data = gsm.getGsmData()
-		key =  "GSM"
-		postToServer(data, url_gsm, key)
+	    print("GSM MODULE")
+	    data = gsm.getGsmData()
+	    key =  "GSM"
+	    postToServer(data, url_gsm, key)
 		
 	if(num == 2): #RFID
-		print("RFID MODULE")
-		data = rfid.getRfidData()
-		key = "RFID"
-		postToServer(data, url_rfid, key)
+	    print("RFID MODULE")
+	    data = rfid.getRfidData()
+	    key = "RFID"
+	    postToServer(data, url_rfid, key)
 	if(num == 3): #ODB2
-		print("ODB2 MODULE")
-		data = odb2.getOdb2Data()
-		postToServer(data, url_odb2)
-	
-
-
-
-def postToServer(data, url, key):
-	data_json = json.dumps(data)
-	payload = data
-	r = requests.post(url, json={key: payload})
-
-
+	    print("ODB2 MODULE")
+            data = obd2.getObd2Data()
+            key="OBD2"  
+            postToServer(data, url_obd2,key) 
 
 
 threads = []
--- obd2.py
+++ obd2.py
@@ -3,9 +3,11 @@ import obd
 def getObd2Data():
     print("ODB2 Data")
     connection = obd.OBD() # auto-connects to USB or RF port
+    if(not connection.is_connected()):
+        print("ODB2 not Connected, please make the connetion")
+        return None
     cmd = obd.commands.SPEED # select an OBD command (sensor)
     response = connection.query(cmd) # send the command, and parse the response
     print(response.value) # returns unit-bearing values thanks to Pint
     print(response.value.to("mph")) # user-friendly unit conversions
 
-getObd2Data()
